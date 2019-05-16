cfc265ea9f8662bde56d1033085b120cca3d6a5f
bintecko <cleberhill@gmail.com>
2019-05-15 23:41:27 -0300

    My Django app, Tirth commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'website'
 ]
 
 MIDDLEWARE = [
@@ -107,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo
 
 USE_I18N = True
 
